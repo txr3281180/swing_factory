477cbf36604b87ed43f001c7b59cb810cf068dd3
xinrui.tian <xinrui.tian@git.sumscope.com>
2018-12-25 17:02:43 +0800

    Initial commit

---
+++ src/main/java/swing/SwingFactoryMain.java
@@ -0,0 +1,11 @@
+package swing;
+
+/**
+ * Created by xinrui.tian on 2018/12/25
+ */
+public class SwingFactoryMain {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/swing/component/DeskTop.java
@@ -0,0 +1,12 @@
+package swing.component;
+
+import javax.swing.JTable;
+
+/**
+ * Created by xinrui.tian on 2018/12/25
+ */
+public class DeskTop {
+
+    private JTable table1;
+
+}
